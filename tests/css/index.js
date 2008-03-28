61356396365ef06096aef0bbcc59a259d534a30c
pilgrim <pilgrim@bb3a0e3a-8946-0410-beb8-7f5728c06e60>
2008-03-28 03:23:51 +0000

    added IE8 test results and CSS property test results
    git-svn-id: http://doctype.googlecode.com/svn/trunk@2075 bb3a0e3a-8946-0410-beb8-7f5728c06e60

--- tests/css/index.js
+++ tests/css/index.js
@@ -1,5 +1,6 @@
 function csssuite() {
     var testSuite = new top.jsUnitTestSuite();
     testSuite.addTestSuite(csscolorsuite());
+    testSuite.addTestSuite(csspropertysuite());
     return testSuite;
 }
