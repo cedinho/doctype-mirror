555e7e1e5dfb9a45b0eb99596b866bd859f59164
pilgrim <pilgrim@bb3a0e3a-8946-0410-beb8-7f5728c06e60>
2008-04-21 15:52:25 +0000

    new version of GoogJSL, updated several wiki articles with new code
    git-svn-id: http://doctype.googlecode.com/svn/trunk@2089 bb3a0e3a-8946-0410-beb8-7f5728c06e60

--- goog/math/coordinate.js
+++ goog/math/coordinate.js
@@ -1,5 +1,5 @@
-// Copyright (c) 2007-2008, Google
-// All rights reserved.
+// Copyright 2006-8 Google Inc.
+// All Rights Reserved.
 // 
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
@@ -25,24 +25,30 @@
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE. 
 
-goog.math = goog.math || {};
+/**
+ * @fileoverview A utility class for representing two-dimensional positions.
+ */
+
+
+goog.provide('goog.math.Coordinate');
+
 
 /**
  * Class for representing coordinates and positions.
- * @param {Number} opt_x Left
- * @param {Number} opt_y Top
+ * @param {number} opt_x Left.
+ * @param {number} opt_y Top.
  * @constructor
  */
 goog.math.Coordinate = function(opt_x, opt_y) {
   /**
    * X-value
-   * @type Number
+   * @type {number|undefined}
    */
   this.x = goog.isDef(opt_x) ? Number(opt_x) : undefined;
 
   /**
    * Y-value
-   * @type Number
+   * @type {number|undefined}
    */
   this.y = goog.isDef(opt_y) ? Number(opt_y) : undefined;
 
@@ -51,7 +57,7 @@ goog.math.Coordinate = function(opt_x, opt_y) {
 
 /**
  * Returns a new copy of the coordinate.
- * @return {goog.math.Coordinate}
+ * @return {goog.math.Coordinate} A clone of this coordinate.
  */
 goog.math.Coordinate.prototype.clone = function() {
   return new goog.math.Coordinate(this.x, this.y);
@@ -59,8 +65,8 @@ goog.math.Coordinate.prototype.clone = function() {
 
 
 /**
- * Returns a nice string representing dimensions.
- * @return {String} In the form (50, 73).
+ * Returns a nice string representing the coordinate.
+ * @return {string} In the form (50, 73).
  */
 goog.math.Coordinate.prototype.toString = function() {
   return '(' + this.x + ', ' + this.y + ')';
@@ -69,9 +75,9 @@ goog.math.Coordinate.prototype.toString = function() {
 
 /**
  * Compares coordinates for equality.
- * @param {goog.math.Coordinate} a
- * @param {goog.math.Coordinate} b
- * @return {Boolean} True iff the coordinates are equal, or if both are null.
+ * @param {goog.math.Coordinate} a A Coordinate.
+ * @param {goog.math.Coordinate} b A Coordinate.
+ * @return {boolean} True iff the coordinates are equal, or if both are null.
  */
 goog.math.Coordinate.equals = function(a, b) {
   if (a == b) {
@@ -86,9 +92,9 @@ goog.math.Coordinate.equals = function(a, b) {
 
 /**
  * Returns the distance between two coordinates.
- * @param {goog.math.Coordinate} a
- * @param {goog.math.Coordinate} b
- * @return {Number}
+ * @param {goog.math.Coordinate} a A Coordinate.
+ * @param {goog.math.Coordinate} b A Coordinate.
+ * @return {number} The distance between {@code a} and {@code b}.
  */
 goog.math.Coordinate.distance = function(a, b) {
   var dx = a.x - b.x;
@@ -104,9 +110,10 @@ goog.math.Coordinate.distance = function(a, b) {
  * Performance note: eliminating the square root is an optimization often used
  * in lower-level languages, but the speed difference is not nearly as
  * pronounced in JavaScript (only a few percent.)
- * @param {goog.math.Coordinate} a
- * @param {goog.math.Coordinate} b
- * @return {Number}
+ *
+ * @param {goog.math.Coordinate} a A Coordinate.
+ * @param {goog.math.Coordinate} b A Coordinate.
+ * @return {number} The squared distance between {@code a} and {@code b}.
  */
 goog.math.Coordinate.squaredDistance = function(a, b) {
   var dx = a.x - b.x;
@@ -118,9 +125,10 @@ goog.math.Coordinate.squaredDistance = function(a, b) {
 /**
  * Returns the difference between two coordinates as a new
  * goog.math.Coordinate.
- * @param {goog.math.Coordinate} a
- * @param {goog.math.Coordinate} b
- * @return {goog.math.Coordinate}
+ * @param {goog.math.Coordinate} a A Coordinate.
+ * @param {goog.math.Coordinate} b A Coordinate.
+ * @return {goog.math.Coordinate} A Coordinate representing the difference
+ *     between {@code a} and {@code b}.
  */
 goog.math.Coordinate.difference = function(a, b) {
   return new goog.math.Coordinate(a.x - b.x, a.y - b.y);
