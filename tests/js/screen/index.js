b23a352b9d0055a0037ee9f9e756b9c9dcad644c
pilgrim <pilgrim@bb3a0e3a-8946-0410-beb8-7f5728c06e60>
2008-05-12 20:25:59 +0000

    added 2 missing tests
    git-svn-id: http://doctype.googlecode.com/svn/trunk@2096 bb3a0e3a-8946-0410-beb8-7f5728c06e60

--- tests/js/screen/index.js
+++ tests/js/screen/index.js
@@ -10,9 +10,11 @@ function jsscreensuite() {
   testSuite.addTestPage("../tests/js/screen/screen-deviceYDPI-typeof-test.html");
   testSuite.addTestPage("../tests/js/screen/screen-fontSmoothingEnabled-typeof-test.html");
   testSuite.addTestPage("../tests/js/screen/screen-height-typeof-test.html");
+  testSuite.addTestPage("../tests/js/screen/screen-left-typeof-test.html");
   testSuite.addTestPage("../tests/js/screen/screen-logicalXDPI-typeof-test.html");
   testSuite.addTestPage("../tests/js/screen/screen-logicalYDPI-typeof-test.html");
   testSuite.addTestPage("../tests/js/screen/screen-pixelDepth-typeof-test.html");
+  testSuite.addTestPage("../tests/js/screen/screen-top-typeof-test.html");
   testSuite.addTestPage("../tests/js/screen/screen-updateInterval-typeof-test.html");
   testSuite.addTestPage("../tests/js/screen/screen-width-typeof-test.html");
   return testSuite;
